87ae67a9a6be444751e64ce16d1fa16f98b4013a
CarlosJimenez-g <carlosjimenez.g2907@gmail.com>
2025-09-15 15:58:08 -0500

    Delete backend directory

--- backend/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-function conexion(){
-    $conn = new mysqli('localhost', 'root', '', 'perutec_bd');
-    if ($conn->connect_error) {
-        die('Error de conexión: ' . $conn->connect_error);
-    }
-    return $conn;
-}
-?>
